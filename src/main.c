008aed28a72fd8d8acf79baba1b2bc09eaffc643
louielouie <louie.desfontaines@gmail.com>
2021-05-13 14:50:13 +0200

    modif color

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ const UWORD palette_table[] =
 	CGB_PAL(25, 31, 23), CGB_PAL(19, 29, 15), CGB_PAL(9, 16, 16), CGB_PAL(4, 4, 6), //sweet green0			//0
 
 	CGB_PAL(25, 31, 23), CGB_PAL(25, 31, 23), CGB_PAL(9, 16, 16), CGB_PAL(4, 4, 6), //sweet green0 sprites	//4
-	CGB_PAL(31, 10, 23), CGB_PAL(31, 26, 31), CGB_PAL(23, 13, 17), CGB_PAL(5, 3, 5), //sweet red0 sprites	//8
+	CGB_PAL(31, 29, 31), CGB_PAL(31, 29, 31), CGB_PAL(20, 13, 14), CGB_PAL(5, 3, 5), //sweet red0 sprites	//8
 
 	CGB_PAL(19, 29, 15), CGB_PAL(19, 29, 15), CGB_PAL(9, 16, 16), CGB_PAL(4, 4, 6), //sweet green1			//12
 	CGB_PAL(9, 16, 16), CGB_PAL(9, 16, 16), CGB_PAL(9, 16, 16), CGB_PAL(4, 4, 6), //sweet green2			//16
@@ -24,7 +24,7 @@ void	main(void) {
 
 	set_bkg_palette(0, 1, &palette_table[0]);
 	set_sprite_palette(0, 1, &palette_table[8]);
-	splash_screen();
+//	splash_screen();
 
 	disable_interrupts();
 	gbt_play(song_Data, 2, 7);
@@ -33,9 +33,9 @@ void	main(void) {
 	set_interrupts(VBL_IFLAG);
 	enable_interrupts();
 
-	HIDE_BKG;
-	title_screen();
-	clear_title_screen();
+//	HIDE_BKG;
+//	title_screen();
+//	clear_title_screen();
 
 	s	fire;
 	s	pl;
